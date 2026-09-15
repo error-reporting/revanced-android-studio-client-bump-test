ee4b869bf2b94369fd097fee9832d55758b309d9
error-reporting <huynhnhuthoang5@gmail.com>
2026-09-15 19:40:15 +0700

    Update ClientType.java

--- extensions/shared/library/src/main/java/app/revanced/extension/shared/spoof/ClientType.java
+++ extensions/shared/library/src/main/java/app/revanced/extension/shared/spoof/ClientType.java
@@ -222,7 +222,6 @@ public enum ClientType {
     ClientType(int id,
                String clientName,
                @Nullable String packageName,
-               String userAgent,
                String deviceMake,
                String deviceModel,
                String osName,
@@ -252,13 +251,13 @@ public enum ClientType {
 
         Locale defaultLocale = Locale.getDefault();
         if (osName == "visionOS" && androidSdkVersion == null) {
-                    userAgent,
+           this.userAgent = String.format("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/18.0 Safari/605.1.15; %s; %s; Build/%s)",
                     packageName,
                     clientVersion,
                     osVersion,
                     deviceModel,
                     defaultLocale
-           );
+            );
         } else if (androidSdkVersion == null) {
             // Convert version from '18.2.22C152' into '18_2_22'
             String userAgentOsVersion = osVersion
