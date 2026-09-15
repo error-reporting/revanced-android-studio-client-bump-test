eeeb77754b86e133db925c5f4f61167095b315b3
error-reporting <huynhnhuthoang5@gmail.com>
2026-09-15 19:48:05 +0700

    Update ClientType.java

--- extensions/shared/library/src/main/java/app/revanced/extension/shared/spoof/ClientType.java
+++ extensions/shared/library/src/main/java/app/revanced/extension/shared/spoof/ClientType.java
@@ -120,7 +120,6 @@ public enum ClientType {
             101,
             "VISIONOS",
             null,
-            "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/18.0 Safari/605.1.15",
             "Apple",
             "RealityDevice14,1",
             "visionOS",
@@ -255,6 +254,7 @@ public enum ClientType {
                     packageName,
                     clientVersion,
                     osVersion,
+                    osName,
                     deviceModel,
                     defaultLocale
             );
