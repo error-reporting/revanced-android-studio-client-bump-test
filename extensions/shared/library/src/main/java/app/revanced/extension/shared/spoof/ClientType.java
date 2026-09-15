5377c80504f4adc7d3c93558ac1a647de1da3172
error-reporting <huynhnhuthoang5@gmail.com>
2026-09-15 16:41:17 +0700

    Update ClientType.java

--- extensions/shared/library/src/main/java/app/revanced/extension/shared/spoof/ClientType.java
+++ extensions/shared/library/src/main/java/app/revanced/extension/shared/spoof/ClientType.java
@@ -24,7 +24,7 @@ public enum ClientType {
             "32",
             "SQ3A.220605.009.A1",
             "132.0.6808.3",
-            "1.73.21",
+            "1.74.19",
             false,
             false,
             "Android VR No auth"
