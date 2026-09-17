12cf95c3239ac62ad9d42494d2ee606bd8b0f957
error-reporting <huynhnhuthoang5@gmail.com>
2026-09-17 16:56:40 +0700

    Update ClientType.java

--- extensions/shared/library/src/main/java/app/revanced/extension/shared/spoof/ClientType.java
+++ extensions/shared/library/src/main/java/app/revanced/extension/shared/spoof/ClientType.java
@@ -15,15 +15,13 @@ public enum ClientType {
     ANDROID_VR_NO_AUTH(
             28,
             "ANDROID_VR",
-            "com.google.android.apps.youtube.vr.oculus",
             "Oculus",
             "Quest 3",
             "Android",
             "12",
-            // Android 12.1
-            "32",
+            "com.google.android.apps.youtube.vr.oculus/1.69.27 (Linux; U; Android 12; Cronet/144.0.7509.3) gzip",
+            "32", // Android 12.1
             "SQ3A.220605.009.A1",
-            "132.0.6808.3",
             "1.69.27",
             false,
             false,
@@ -34,61 +32,71 @@ public enum ClientType {
     ANDROID_UNPLUGGED(
             29,
             "ANDROID_UNPLUGGED",
-            "com.google.android.apps.youtube.unplugged",
             "Google",
             "Google TV Streamer",
             "Android",
             "14",
+            "com.google.android.apps.youtube.unplugged/10.34.0 (Linux; U; Android 14; Cronet/151.0.7922.83) gzip",
             "34",
             "UTTK.260317.003",
-            "151.0.7922.83",
             "10.34.0",
             true,
             true,
             "Android TV"
     ),
     // Cannot play livestreams and lacks HDR, but can play videos with music and labeled "for children".
-    // Google Pixel 9 Pro Fold
-    // https://dumps.tadiphone.dev/dumps/google/barbet
     ANDROID_CREATOR(
             14,
             "ANDROID_CREATOR",
-            "com.google.android.apps.youtube.creator",
-            "Google",
-            "Pixel 9 Pro Fold",
+            Build.MANUFACTURER,
+            Build.MODEL,
             "Android",
-            "15",
-            "35",
-            "AP3A.241005.015.A2",
-            "144.0.7509.3",
+            "13",
+            "com.google.android.apps.youtube.creator/24.45.100 (Linux; U; Android 13; Cronet/144.0.7509.3) gzip",
+            "33",
+            Build.ID,
             "25.51.100",
             true,
             true,
-            "Android Studio"
+            "Android Creator"
+    ),
+    ANDROID_VR(
+            ANDROID_VR_NO_AUTH.id,
+            ANDROID_VR_NO_AUTH.clientName,
+            ANDROID_VR_NO_AUTH.deviceMake,
+            ANDROID_VR_NO_AUTH.deviceModel,
+            ANDROID_VR_NO_AUTH.osName,
+            ANDROID_VR_NO_AUTH.osVersion,
+            ANDROID_VR_NO_AUTH.userAgent,
+            ANDROID_VR_NO_AUTH.androidSdkVersion,
+            ANDROID_VR_NO_AUTH.buildId,
+            ANDROID_VR_NO_AUTH.clientVersion,
+            true,
+            true,
+           "Android VR"
     ),
     IOS_UNPLUGGED(
             33,
             "IOS_UNPLUGGED",
-            "com.google.ios.youtubeunplugged",
             "Apple",
             forceAVC()
-                    // 11 Pro Max (last device with iOS 13)
-                    ? "iPhone12,5"
-                    // 15 Pro Max
-                    : "iPhone16,2",
+                    ? "iPhone12,5"  // 11 Pro Max (last device with iOS 13)
+                    : "iPhone16,2", // 15 Pro Max
             "iOS",
+            // iOS 13 and earlier uses only AVC. 14+ adds VP9 and AV1.
             forceAVC()
-                    // iOS 13 and earlier uses only AVC. 14+ adds VP9 and AV1.
-                    ? "13.7.17H35"
+                    ? "13.7.17H35" // Last release of iOS 13.
                     : "18.2.22C152",
-            null,
+            forceAVC()
+                    ? "com.google.ios.youtubeunplugged/6.45 (iPhone12,5; U; CPU iOS 13_7 like Mac OS X)"
+                    : "com.google.ios.youtubeunplugged/8.49 (iPhone16,2; U; CPU iOS 18_2_22 like Mac OS X)",
             null,
             null,
             // Version number should be a valid iOS release.
             // https://www.ipa4fun.com/history/152043/
+            // Some newer versions can also force AVC,
+            // but 6.45 is the last version that supports iOS 13.
             forceAVC()
-                    // Some newer versions can also force AVC,
-                    // but 6.45 is the last version that supports iOS 13.
                     ? "6.45"
                     : "8.49",
             true,
@@ -96,43 +104,7 @@ public enum ClientType {
             forceAVC()
                     ? "iOS TV Force AVC"
                     : "iOS TV"
-    ),
-    ANDROID_VR_AUTH(
-            ANDROID_VR_NO_AUTH.id,
-            ANDROID_VR_NO_AUTH.clientName,
-            ANDROID_VR_NO_AUTH.packageName,
-            ANDROID_VR_NO_AUTH.deviceMake,
-            ANDROID_VR_NO_AUTH.deviceModel,
-            ANDROID_VR_NO_AUTH.osName,
-            ANDROID_VR_NO_AUTH.osVersion,
-            ANDROID_VR_NO_AUTH.androidSdkVersion,
-            ANDROID_VR_NO_AUTH.buildId,
-            ANDROID_VR_NO_AUTH.cronetVersion,
-            ANDROID_VR_NO_AUTH.clientVersion,
-            ANDROID_VR_NO_AUTH.requiresAuth,
-            true,
-            "Android VR Auth"
-    ),
-     /**
-     * Internal YT client for an unreleased YT client. May stop working at any time.
-     */
-      VISIONOS(
-            101,
-            "VISIONOS",
-            "com.google.visionosyoutube",
-            "Apple",
-            "RealityDevice14,1",
-            "visionOS",
-            "26.6.1",
-            null,
-            null,
-            null,
-            "1.02",
-            false,
-            false,
-            "visionOS 1.02"
     );
-
 
     private static boolean forceAVC() {
         return BaseSettings.SPOOF_VIDEO_STREAMS_IOS_FORCE_AVC.get();
@@ -145,17 +117,6 @@ public enum ClientType {
     public final int id;
 
     public final String clientName;
-
-    /**
-     * App package name.
-     */
-    @Nullable
-    private final String packageName;
-
-    /**
-     * Player user-agent.
-     */
-    public final String userAgent;
 
     /**
      * Device model, equivalent to {@link Build#MANUFACTURER} (System property: ro.product.vendor.manufacturer)
@@ -178,26 +139,22 @@ public enum ClientType {
     public final String osVersion;
 
     /**
+     * Player user-agent.
+     */
+    public final String userAgent;
+
+    /**
      * Android SDK version, equivalent to {@link Build.VERSION#SDK} (System property: ro.build.version.sdk)
      * Field is null if not applicable.
      */
     @Nullable
     public final String androidSdkVersion;
-
     /**
      * Android build id, equivalent to {@link Build#ID}.
      * Field is null if not applicable.
      */
     @Nullable
     private final String buildId;
-
-    /**
-     * Cronet release version, as found in decompiled client apk.
-     * Field is null if not applicable.
-     */
-    @Nullable
-    private final String cronetVersion;
-
     /**
      * App version.
      */
@@ -219,73 +176,34 @@ public enum ClientType {
      */
     public final String friendlyName;
 
-    @SuppressWarnings("ConstantLocale")
     ClientType(int id,
                String clientName,
-               @Nullable String packageName,
                String deviceMake,
                String deviceModel,
                String osName,
                String osVersion,
+               String userAgent,
                @Nullable String androidSdkVersion,
-               @Nullable String buildId,
-               @Nullable String cronetVersion,
+               @Nullable String buildId
                String clientVersion,
                boolean requiresAuth,
                boolean useAuth,
                String friendlyName) {
         this.id = id;
         this.clientName = clientName;
-        this.packageName = packageName;
         this.deviceMake = deviceMake;
         this.deviceModel = deviceModel;
         this.osName = osName;
         this.osVersion = osVersion;
+        this.userAgent = userAgent;
         this.androidSdkVersion = androidSdkVersion;
         this.buildId = buildId;
-        this.cronetVersion = cronetVersion;
         this.clientVersion = clientVersion;
         this.requiresAuth = requiresAuth;
         this.useAuth = useAuth;
         this.friendlyName = friendlyName;
-
         Locale defaultLocale = Locale.getDefault();
-        
-        if (osName == "visionOS" && androidSdkVersion == null) {
-             String userAgentOsVersion = osVersion
-                    .replaceAll("(\\d+\\.\\d+\\.\\d+).*", "$1")
-                    .replace(".", "_");
-            this.userAgent = String.format("%s/%s (%s; U; CPU visionOS %s like Mac OS X; %s; Build/23O780) gzip",
-                    packageName,
-                    clientVersion,
-                    deviceModel,
-                    userAgentOsVersion,
-                    defaultLocale
-        );
-        } else if (androidSdkVersion == null) {
-            // Convert version from '18.2.22C152' into '18_2_22'
-            String userAgentOsVersion = osVersion
-                    .replaceAll("(\\d+\\.\\d+\\.\\d+).*", "$1")
-                    .replace(".", "_");
-            // https://github.com/mitmproxy/mitmproxy/issues/4836
-            this.userAgent = String.format("%s/%s (%s; U; CPU iOS %s like Mac OS X; %s)",
-                    packageName,
-                    clientVersion,
-                    deviceModel,
-                    userAgentOsVersion,
-                    defaultLocale
-            );
-        } else {
-            this.userAgent = String.format("%s/%s (Linux; U; Android %s; %s; %s; Build/%s; Cronet/%s)",
-                    packageName,
-                    clientVersion,
-                    osVersion,
-                    defaultLocale,
-                    deviceModel,
-                    Objects.requireNonNull(buildId),
-                    Objects.requireNonNull(cronetVersion)
-            );
-        }
         Logger.printDebug(() -> "userAgent: " + this.userAgent);
     }
+
 }
