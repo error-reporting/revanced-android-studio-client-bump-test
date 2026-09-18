9601dffa7f35e2cbe6cc82d705d05eec21442156
error-reporting <huynhnhuthoang5@gmail.com>
2026-09-18 13:26:57 +0700

    Update YouTubeClient.kt

--- extensions/shared/src/main/java/app/revanced/extension/shared/innertube/client/YouTubeClient.kt
+++ extensions/shared/src/main/java/app/revanced/extension/shared/innertube/client/YouTubeClient.kt
@@ -264,7 +264,7 @@ object YouTubeClient {
         "$packageName/$clientVersion(Linux; U; Android $osVersion; ${Locale.getDefault()}; $deviceModel Build/$buildId) gzip"
 
     private fun useAV1(): Boolean {
-        return BaseSettings.SPOOF_STREAMING_DATA_ANDROID_VR_ENABLE_AV1_CODEC.get() || BaseSettings.SPOOF_STREAMING_DATA_VISIONOS_ENABLE_AV1_CODEC.get()
+        return BaseSettings.SPOOF_STREAMING_DATA_ANDROID_VR_ENABLE_AV1_CODEC.get() || return BaseSettings.SPOOF_STREAMING_DATA_VISIONOS_ENABLE_AV1_CODEC.get()
     }
 
     private fun useJS(): Boolean {
